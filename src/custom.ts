3780b6dee4f04fceb4ff91cf9a7206e5d0f5eaf8
songjj <shongjj@meleapcn.com>
2026-03-13 18:50:35 +0800

    Add click event handler in custom.ts to manage link navigation
    Implement a hookClick function to intercept click events on anchor tags, preventing default behavior for links targeting _blank or when a base target is set. Enhance logging for better debugging and maintainability.

--- src/custom.ts
+++ src/custom.ts
@@ -25,3 +25,30 @@ contextBridge.exposeInMainWorld('electronAPI', {
 window.addEventListener('DOMContentLoaded', () => {
     console.log('Preload script loaded')
 })
+
+console.log(
+    '%cbuild from PakePlus https://github.com/Sjj1024/PakePlus',
+    'color:orangered;font-weight:bolder'
+)
+
+// very important, if you don't know what it is, don't touch it
+// 非常重要，不懂代码不要动
+const hookClick = (e: MouseEvent) => {
+    const origin = (e.target as HTMLElement | null)?.closest('a')
+    const isBaseTargetBlank = document.querySelector(
+        'head base[target="_blank"]'
+    )
+    console.log('origin', origin, isBaseTargetBlank)
+    if (
+        (origin && origin.href && origin.target === '_blank') ||
+        (origin && origin.href && isBaseTargetBlank)
+    ) {
+        e.preventDefault()
+        console.log('handle origin', origin)
+        location.href = origin.href
+    } else {
+        console.log('not handle origin', origin)
+    }
+}
+
+document.addEventListener('click', hookClick, { capture: true })
